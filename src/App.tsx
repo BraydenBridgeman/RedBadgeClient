c94ebc3f045e486e245bfc60939f6e14fd4e5c4c
Brayden Bridgeman <braydenbridgeman@gmail.com>
2022-01-27 21:57:30 -0500

    basic structure started in client

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from 'react-router-dom';
+
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import Login from './Auth/Login';
+// import SignIn from './Auth//SignIn';
+// import RegisterUser from './Auth//RegisterUser';
+// import PublicLists from './Components/PublicLists';
+// import Search from './Components/Search';
+// import SiteNav from './Components/SiteNav';
+// import UserLists from './Components/UserLists';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            {/* <Login />
+            <PublicLists />
+            <RegisterUser />
+            <Search />
+            <SignIn />
+            <SiteNav />
+            <UserLists /> */}
+        </div>
+    );
 }
 
 export default App;
--- src/Components/SiteNav.tsx
+++ src/Components/SiteNav.tsx
@@ -1,1 +1,27 @@
-export {}
+import React, {useState, useEffect} from 'react';
+
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from 'react-router-dom';
+
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem
+} from 'reactstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './SiteNav.css';
+
+const SiteNav = (props: any) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default SiteNav;
