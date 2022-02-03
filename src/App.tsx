ba5927fb20b59cafcdb170fe1ffa6feb9ce00315
Brayden Bridgeman <braydenbridgeman@gmail.com>
2022-02-02 21:35:44 -0500

    started API creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import {
-    BrowserRouter,
-    Routes,
-    Route
-} from 'react-router-dom';
+import React from 'react';
+// import {
+//     BrowserRouter,
+//     Routes,
+//     Route
+// } from 'react-router-dom';
 
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import Login from './Auth/Login';
-// import SignIn from './Auth//SignIn';
-// import RegisterUser from './Auth//RegisterUser';
-// import PublicLists from './Components/PublicLists';
+// import SignIn from './Auth/SignIn';
+// import RegisterUser from './Auth/RegisterUser';
+// import PublicLists from './Home/PublicLists';
 // import Search from './Components/Search';
-// import SiteNav from './Components/SiteNav';
+// import SiteNav from './Home/SiteNav';
 // import UserLists from './Components/UserLists';
 
+interface MovieAPI {
+    movieName: string,
+    yearReleased: string,
+    genre: string,
+    shortPlot: string,
+    moviePoster: string
+}
+
+function getMovies(): Promise<MovieAPI[]> {
+    return fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.REACT_APP_API_KEY}`)
+    .then(res => res.json())
+    .then(res => {
+        return res as MovieAPI[]
+    })
+}
+
+getMovies();
+
 function App() {
-    return (
+
+
+   return (
         <div className="App">
-            {/* <Login />
-            <PublicLists />
-            <RegisterUser />
-            <Search />
-            <SignIn />
-            <SiteNav />
-            <UserLists /> */}
+
         </div>
     );
 }
 
 export default App;
+
+// {/* <Login />
+//             <PublicLists />
+//             <RegisterUser />
+//             <Search />
+//             <SignIn />
+//             <SiteNav />
+//             <UserLists /> */}
