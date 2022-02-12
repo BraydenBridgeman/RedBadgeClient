307b9b82dbbbc80d581056825c2dac74df2e0cb4
Brayden Bridgeman <braydenbridgeman@gmail.com>
2022-02-12 13:43:32 -0500

    push for testing api calls

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,8 @@ interface MovieAPI {
 }
 
 function App() {
-    const [movies, setMovies] = useState([]);
+    const [movies, setMovies] = useState<object[]>([]);
+    const [testMovies, setTestMovies] = useState<any>('');
     const [targetMovie, setTargetMovie] = useState({});
     const [searchValue, setSearchValue] = useState('');
     const [publicLists, setPublicLists] = useState([]);
@@ -34,7 +35,7 @@ function App() {
         }
     }, []);
 
-    const updateToken = (newToken : string) => {
+    const updateToken = (newToken: string) => {
         console.log('updateToken');
         localStorage.setItem('token', newToken);
         setSessionToken(newToken);
@@ -48,54 +49,66 @@ function App() {
 
     // Calling API for Movie Name, Year Released, Movie Poster, Genre, Short Plot
     const getMovieList = async () => {
-            // First API Call
-            const res = await(await fetch(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${searchValue}`)).json()
-            console.log(res);
-    
-            // Movie Name
-            // const movieName = res.Search[0].Title;
-            // console.log(movieName);
-    
-            // // Year Released
-            // const yearReleased = res.Search[0].Year;
-            // console.log(yearReleased);
-    
-            // // Movie Poster
-            // const moviePoster = res.Search[0].Poster;
-            // console.log(moviePoster);
+        // First API Call
+        const res = await (await fetch(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${searchValue}`)).json()
+        console.log(res);
 
-            // Second API Call
-            // for(let i=0; i<res.Search.length; i++) {
-            //     const apiRES = await(await fetch(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&t=${res.Search[i].Title}&y=${res.Search[i].Year}`)).json()
-            //     console.log(apiRES);
-            // }
-            // setTargetMovie(apiRES);
-    
-            // // Genre
-            // const genre = apiRES.Genre;
-            // console.log(genre);
-            
-            // // Short Plot
-            // const shortPlot = apiRES.Plot;
-            // console.log(shortPlot);
-    
-            if (res.Search) {
-                setMovies(res.Search);
+        // Movie Name
+        // const movieName = res.Search[0].Title;
+        // console.log(movieName);
+
+        // // Year Released
+        // const yearReleased = res.Search[0].Year;
+        // console.log(yearReleased);
+
+        // // Movie Poster
+        // const moviePoster = res.Search[0].Poster;
+        // console.log(moviePoster);
+
+        // Second API Call
+        if (res.Search) {
+            setMovies(res.Search);
+            for (let i = 0; i < movies.length; i++) {
+                const apiRES: {
+                    Genre: string,
+                    Plot: string
+                } = await (await fetch(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&t=${res.Search[i].Title}&y=${res.Search[i].Year}`)).json()
+                console.log(apiRES);
+                // setTestMovies("test movies test");
+                setTestMovies([
+                    ...movies, {
+                        ...movies[i],
+                        Genre: apiRES.Genre,
+                        Plot: apiRES.Plot
+                    }
+                ])
             }
-            // } else if (apiRES.Genre) {
-            //     setMovies(apiRES.Genre)
-            // } else if (apiRES.Plot) {
-            //     setMovies(apiRES.Plot);
-            // }
+        }
+        // setTargetMovie(apiRES);
+
+        // // Genre
+        // const genre = apiRES.Genre;
+        // console.log(genre);
+
+        // // Short Plot
+        // const shortPlot = apiRES.Plot;
+        // console.log(shortPlot);
+
+        // } else if (apiRES.Genre) {
+        //     setMovies(apiRES.Genre)
+        // } else if (apiRES.Plot) {
+        //     setMovies(apiRES.Plot);
+        // }
     }
 
     useEffect(() => {
-        if (searchValue){
+        console.log(testMovies);
+        if (searchValue) {
             getMovieList();
         }
     }, [searchValue]);
 
-   return (
+    return (
         <div className="App">
             <SiteNav setSearchValue={setSearchValue} sessionToken={sessionToken} tokenUpdate={updateToken} logout={clearToken} />
             <Auth tokenUpdate={updateToken} />
