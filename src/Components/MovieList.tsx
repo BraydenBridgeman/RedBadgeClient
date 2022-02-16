62fc622f89e613b492273f117fef1fba91fab1ab
Brayden Bridgeman <braydenbridgeman@gmail.com>
2022-02-16 18:29:44 -0500

    add movie button completed.

--- src/Components/MovieList.tsx
+++ src/Components/MovieList.tsx
@@ -1,45 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import APIURL from '../Helpers/environments';
+import { Row, Col, Card } from 'reactstrap';
 
-const MovieList = (props : any) => {
+import AddMovies from '../PublicList/AddMovies';
+
+const MovieList = (props: any) => {
 
     const [allMovies, setAllMovies] = useState([]);
 
     useEffect(() => {
-        getMovies();
-    }, [])
-
-    const getMovies = () => {
-        if (props.token !== '') {
-            console.log(APIURL);
-            fetch(`${APIURL}/allMovies/getMovies`, {
-                method: 'GET',
-                headers: new Headers({
-                    'Content-Type' : 'application/JSON'
+        const getMovies = () => {
+            if (props.token !== '') {
+                console.log(APIURL);
+                fetch(`${APIURL}/allMovies/getMovies`, {
+                    method: 'GET',
+                    headers: new Headers({
+                        'Content-Type': 'application/JSON'
+                    })
                 })
-            })
-            .then(response => response.json())
-            .then(data => setAllMovies(data))
+                    .then(response => response.json())
+                    .then(data => setAllMovies(data))
+            }
         }
-    }
+        getMovies();
+    }, [allMovies, props.token])
 
-    return(
-        <>
-            {props.movies.map
-            ((movie: { Poster: string | undefined; 
-            Title: string; 
-            Year: number; 
-            Genre: string; 
-            Plot: string; }, index: any) => 
-            <div>
-                <img src={movie.Poster} alt="movie poster"></img>
-                <h2>{movie.Title}</h2>
-                <h4>{movie.Year}</h4>
-                <p>{movie.Genre}</p>
-                <p>{movie.Plot}</p>
-            </div>
-            )}
-        </>
+
+    return (
+        <div className="searchMovieList">
+            <Row xs="1" sm="2" md="4">
+                <Col className="bg-light border" xs="6">
+                    <Card className="movieListCard" style={{ width: "100%" }}>
+                        {props.movies.map
+                            ((movie: {
+                                Poster: string | undefined;
+                                Title: string;
+                                Year: number;
+                                Genre: string;
+                                Plot: string;
+                            }, _index: any) =>
+                                <div>
+                                    <img style={{ width: "250px"}} src={movie.Poster} alt="movie poster"></img>
+                                    <h2>{movie.Title}</h2>
+                                    <h4>{movie.Year}</h4>
+                                    <p>{movie.Genre}</p>
+                                    <p>{movie.Plot}</p>
+                                    <AddMovies movies = {props.movies} token={props.sessionToken} movieAdd = {props.movieToAdd} />
+                                </div>
+                            )}
+                    </Card>
+                </Col>
+            </Row>
+        </div>
     );
 };
 
--- src/PublicList/AddMovies.tsx
+++ src/PublicList/AddMovies.tsx
@@ -1,53 +1,48 @@
-// import React from 'react';
-// import { Button } from 'reactstrap';
+import React from 'react';
+import { Button } from 'reactstrap';
 
-// import APIURL from '../Helpers/environments';
+import APIURL from '../Helpers/environments';
 
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './AddMovies.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './AddMovies.css';
 
-// const AddMovies = (props: any) => {
+const AddMovies = (props: any) => {
 
-//     let movieAdd = (id: number) => {
-//         let arrayId = id - 1;
-//         let movie = props.movies[arrayId];
-//         let movieName = movie.Title;
-//         let yearReleased = movie.Year;
-//         let moviePoster = movie.Poster;
-//         let genre = movie.Genre;
-//         let shortPlot = movie.Plot;
-//         console.log(movie);
-//         console.log(movieName, yearReleased, moviePoster, genre, shortPlot, props.sessionToken);
+    let movieAdd = (id: number) => {
+        let arrayId = id - 1;
+        let movie = props.movies[arrayId];
+        let movieName = movie.Title;
+        let yearReleased = movie.Year;
+        let moviePoster = movie.Poster;
+        let genre = movie.Genre;
+        let shortPlot = movie.Plot;
+        console.log(movie);
+        console.log(movieName, yearReleased, moviePoster, genre, shortPlot, props.sessionToken);
 
-//         handleSubmit = (event : any) => {
-//             event.preventDefault();
-//             console.log(APIURL);
-//             fetch(`${APIURL}/movies/movies`, {
-//                 method: "POST",
-//                 body: JSON.stringify({
-//                     movies: {
-//                         movieName: movieName,
-//                         yearReleased: yearReleased,
-//                         genre: genre,
-//                         shortPlot: shortPlot,
-//                         moviePoster: moviePoster
-//                     }
-//                 }),
-//                 headers: new Headers({
-//                     'Content-Type': 'application/json',
-//                     'Authorization': `Bearer ${props.sessionToken}`,
-//                 })
-//             }).then((response) => response.json())
-//         }
-//     }
+        fetch(`${APIURL}/movies/movies`, {
+            method: "POST",
+            body: JSON.stringify({
+                movies: {
+                    movieName: movieName,
+                    yearReleased: yearReleased,
+                    genre: genre,
+                    shortPlot: shortPlot,
+                    moviePoster: moviePoster
+                }
+            }),
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${props.sessionToken}`,
+            })
+        }).then((response) => response.json())
+    }
 
-//     return (
-//         <Button onSubmit={e => {e.preventDefault(); handleSubmit(e)}} className="addMovieToList" onClick={(e) = movieAdd(props.movieToAdd)}>
-//             Add Movie to My List
-//         </Button>
-//     )
+    return (
+        <Button className="addMoviesButton" onClick={() => movieAdd(props.movieToAdd)}>
+            Add Movie to My List
+        </Button>
+    )
 
-// }
+}
 
-// export default AddMovies;
-export {};
+export default AddMovies;
